6d61a5424376ab89f97dc5be47c01e690916802c
Michael Mafeni <michael.mafeni@automation-consultants.com>
2021-04-13 16:13:57 +0100

    Add types declaration

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+import {CQLTerm, CQLAtom, CQLSingleAtom, CQLListAtom, BinOp, UnOp} from "./cql/parser/types";
+
+export {
+    CQLTerm,
+    CQLSingleAtom,
+    CQLListAtom,
+    CQLAtom,
+    BinOp,
+    UnOp
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { C, F, N, Streams } from "@masala/parser";
-import { parseCql } from "./cql/parser/parser";
+import {parseCql} from "./cql/parser/parser";
+import { CQL_BINARY_OPERATORS, CQL_FIELDS, CQL_LIST_OPERATORS, CQL_STRING_OPERATORS, CQL_UNARY_OPERATORS } from "./cql/parser/constants";
 
-const testParser = C.string("help").flatMap(val => C.string("123").returns(val + "hello"));
-
-console.log(testParser.parse(Streams.ofString("help123")));
+export {
+    parseCql,
+    CQL_BINARY_OPERATORS,
+    CQL_FIELDS,
+    CQL_LIST_OPERATORS,
+    CQL_STRING_OPERATORS,
+    CQL_UNARY_OPERATORS
+};
